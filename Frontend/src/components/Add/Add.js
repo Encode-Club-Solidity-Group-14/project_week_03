cc0762d2029b30b8ed304a388f0f299a806359f4
kayceedev <Nwobodoe71@gmail.com>
2022-07-22 19:59:15 +0100

    Refactor Add file
    Signed-off-by: kayceedev <Nwobodoe71@gmail.com>

--- Frontend/src/components/Add/Add.js
+++ Frontend/src/components/Add/Add.js
@@ -1,167 +1,153 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./Add.module.css";
-import axios from 'axios'
-import Card from '../UI/Card/Card';
-import Button from '../UI/Button/Button';
-import MainHeader from '../MainHeader/MainHeader';
+import axios from "axios";
+import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
+import MainHeader from "../MainHeader/MainHeader";
+import App from "../image-upload/image";
 
 function Add() {
+  const [metadata, setMetadata] = useState({
+    background: "",
+    left_eye: "",
+    face: "",
+    right_eye: "",
+    mouth: "",
+    accessory: "",
+    score: "",
+  });
+  const [imageFiles, setImageFiles] = useState([]);
+  const [status, setStatus] = useState(false);
+  const navigate = useNavigate()
 
-    const [typedBackground, setTypedBackground] = useState("backout");
-    const [typedLeftEye, setTypedLeftEye] = useState("left_eye");
-    const [typedface, setTypedfaced] = useState("face");
-    const [typedRightEye, setTypedRightEye] = useState("right_eye");
-    const [typedMouth, setTypedMouth] = useState("mouth");
-    const [typedAccessory, setTypedAccessory] = useState("accessory");
-    const [typedScore, setScore] = useState("score");
+  useEffect(() => {
+    if (status) {
+      navigate("/list");
+    }
+  }, [status, navigate]);
 
-  const backgroundChangeHandler = (event) => {
-    setTypedBackground(event.target.value);
-  }
-
-  const leftEyeChangeHandler = (event) => {
-    setTypedLeftEye(event.target.value);
-  }
-
-  const faceChangeHandler = (event) => {
-    setTypedfaced(event.target.value);
-  }
-
-
-  const rightEyeChangeHandler = (event) => {
-    setTypedRightEye(event.target.value);
-  }
-
-
-  const mouthChangeHandler = (event) => {
-    setTypedMouth(event.target.value);
-  }
-
-
-  const accessoryChangeHandler = (event) => {
-    setTypedAccessory(event.target.value);
-  }
-
-
-  const scoreChangeHandler = (event) => {
-    setScore(event.target.value);
-  }
-
-
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setMetadata({ [name]: value });
+  };
 
   const submitHandler = async (event) => {
     event.preventDefault();
-    const metadata = {
-        "metadata": {
-            "background": typedBackground,
-            "left_eye": typedLeftEye, 
-            "face": typedface,
-            "right_eye": typedRightEye,
-            "mouth": typedMouth,
-            "accessory": typedAccessory,
-            "score": typedScore
-        }
+    const { face, background, left_eye, right_eye, mouth, accessory, score } =
+      metadata;
+    if (
+      (face,
+      mouth,
+      background,
+      left_eye,
+      right_eye,
+      metadata,
+      accessory,
+      score,
+      imageFiles.length > 0)
+    ) {
+      const metadataResponse = await axios.post(
+        `${process.env.REACT_APP_SERVER_IP}/nfts`,
+        metadata
+      );
+      if (metadataResponse.data) {
+        console.log(typeof metadataResponse.data);
+        const uploadImage = await axios.post(
+          `${process.env.REACT_APP_SERVER_IP}/nfts/${metadataResponse.data}/image`,
+          imageFiles
+        );
+        alert("NFT created! Id: " + uploadImage.id);
+      }
     }
-
-    const id = await axios.post("http://localhost:3001/nfts", metadata);
-
-    alert("NFT created! Id: " + id.data);
-
-
-    const uploadImage = await axios.post("http://localhost:3001/nfts/"+id.data+"image", metadata);
-  }
+  };
 
   return (
     <React.Fragment>
-    <MainHeader />
-    <Card className={classes.form}>
-    <form onSubmit={submitHandler}>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="background">Background</label>
-        <input
-          type="background"
-          id="background"
-          onChange={backgroundChangeHandler}
-          value={typedBackground}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="lefteye">Left Eye</label>
-        <input
-          type="lefteye"
-          id="lefteye"
-          onChange={leftEyeChangeHandler}
-          value={typedLeftEye}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="face">Face</label>
-        <input
-          type="face"
-          id="face"
-          onChange={faceChangeHandler}
-          value={typedface}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="rightEye">Right Eye</label>
-        <input
-          type="rightEye"
-          id="rightEye"
-          onChange={rightEyeChangeHandler}
-          value={typedRightEye}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="mouth">Background</label>
-        <input
-          type="mouth"
-          id="mouth"
-          onChange={mouthChangeHandler}
-          value={typedMouth}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="acessory">Acessory</label>
-        <input
-          type="acessory"
-          id="acessory"
-          onChange={accessoryChangeHandler}
-          value={typedAccessory}
-        />
-      </div>
-      <div
-        className={`${classes.control}`}
-      >
-        <label htmlFor="score">Score</label>
-        <input
-          type="score"
-          id="score"
-          onChange={scoreChangeHandler}
-          value={typedScore}
-        />
-      </div>
-            <div className={classes.actions}>
-        <Button type="submit" className={classes.btn}>
-          Create NFT 
-        </Button>
-      </div>
-    </form>
-  </Card>
-  </React.Fragment>
-  )
+      <MainHeader />
+      <Card className={classes.form}>
+        <form onSubmit={submitHandler}>
+          <div className={`${classes.control}`}>
+            <label htmlFor="background">Background</label>
+            <input
+              type="text"
+              id="background"
+              name="background"
+              onChange={onChangeHandler}
+              value={metadata.background}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="lefteye">Left Eye</label>
+            <input
+              type="text"
+              id="left_eye"
+              name="left_eye"
+              onChange={onChangeHandler}
+              value={metadata.left_eye}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="face">Face</label>
+            <input
+              type="text"
+              id="face"
+              name="face"
+              onChange={onChangeHandler}
+              value={metadata.face}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="rightEye">Right Eye</label>
+            <input
+              type="text"
+              id="right_eye"
+              name="rigt_eye"
+              onChange={onChangeHandler}
+              value={metadata.right_eye}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="mouth">Background</label>
+            <input
+              type="text"
+              id="mouth"
+              name="mouth"
+              onChange={onChangeHandler}
+              value={metadata.mouth}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="acessory">Acessory</label>
+            <input
+              type="text"
+              id="acessory"
+              name="acessory"
+              onChange={onChangeHandler}
+              value={metadata.accessory}
+            />
+          </div>
+          <div className={`${classes.control}`}>
+            <label htmlFor="score">Score</label>
+            <input
+              type="text"
+              id="score"
+              name="score"
+              onChange={onChangeHandler}
+              value={metadata.score}
+            />
+          </div>
+          <div className={classes.actions}>
+            <App imageFiles={imageFiles} setImageFiles={setImageFiles} />
+            <Button type="submit" className={classes.btn}>
+              Create NFT
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </React.Fragment>
+  );
 }
 
-export default Add
+export default Add;
